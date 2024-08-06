6ef7e12e0dddd04be653a64cf2efca181cb51c93
pxluhor <pxluthor@gmail.com>
2024-08-06 19:40:13 -0300

    teste.1

--- site_transcription.py
+++ site_transcription.py
@@ -38,7 +38,7 @@ model = 'whisper-large-v3'
 
 genai.configure(api_key=api_key_gemini)
 generation_config = {
-    "temperature": 0.5,
+    "temperature": 0.3,
     "top_p": 0.95,
     "top_k": 64,
     "max_output_tokens": 8192,
@@ -54,7 +54,7 @@ safety_settings={
 model_g = genai.GenerativeModel(model_name='models/gemini-1.5-flash-latest', generation_config=generation_config, safety_settings=safety_settings)
 
 # Função para dividir o áudio
-def split_audio(filepath, chunk_length_ms=120000): # 3 minutos
+def split_audio(filepath, chunk_length_ms=120000): # 2 minutos
     audio = AudioSegment.from_file(filepath)  # segmento de audio
     chunks = [audio[i:i + chunk_length_ms] for i in range(0, len(audio), chunk_length_ms)]
     return chunks
